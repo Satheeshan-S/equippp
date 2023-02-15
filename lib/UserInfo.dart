2001efdf590fb170722032e4c80a00602ac64515
Satheeshan-S <satheeshan.satheesh26@gmail.com>
2023-02-15 16:31:28 +0530

    Completed t

--- lib/UserInfo.dart
+++ lib/UserInfo.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-class Uapp extends StatelessWidget{
+
+class Uapp extends StatelessWidget {
   const Uapp({super.key});
+
   @override
   Widget build(BuildContext context) {
-   return const MaterialApp(
-     home: Scaffold(
-       body: UserInformation(),
-     ),
-   );
-  }}
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: UserInformation(),
+      ),
+    );
+  }
+}
+
 class UserInformation extends StatefulWidget {
   const UserInformation({Key? key}) : super(key: key);
 
@@ -19,6 +24,8 @@ class UserInformation extends StatefulWidget {
 class _UserInformationState extends State<UserInformation> {
   @override
   Widget build(BuildContext context) {
-    return const Text('logged in successfully');
+    return const Center(
+      child: Text('logged in successfully'),
+    );
   }
 }
